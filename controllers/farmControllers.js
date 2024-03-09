77672291c060681df113a57c4610d7f7cdf98f61
ledavto <ser_ega@ukr.net>
2024-03-10 00:15:08 +0200

    Update farmControllers.js

--- controllers/farmControllers.js
+++ controllers/farmControllers.js
@@ -19,7 +19,7 @@ const fetchFarms = ctrlWrapper(async (req, res) => {
 
   const listFarms = await Farm.find({});
 
-  res.status(201).json({listFarms});
+  res.status(201).json({...listFarms});
 });
 
 module.exports = {addFarm, fetchFarms};
